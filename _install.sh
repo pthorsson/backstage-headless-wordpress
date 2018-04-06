61b511dbdb61b19cfee1173b0eb00cbd1b1502aa
pthorsson <p.thorsson@gmail.com>
2018-04-06 23:53:42 +0200

    Install script can now run all the way

--- _install.sh
+++ _install.sh
@@ -24,7 +24,66 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 # WP-CLI - Download WordPress
 _HLWP_LOG "Downloading WordPress version 4.9.4 ..."
-php wp-cli.phar core download --version=4.9.4 --locale=en_US --force
+php wp-cli.phar core download --version=4.9.4 --locale=en_US --force 2> error.log
+_HLWP_KILL_IF_ERROR "Could not download WordPress"
+
+# WP-CLI - Creating wp-config file
+_HLWP_LOG "Creating wp-config ..."
+php wp-cli.phar core config --dbname="$HLWP_WP_DB_NAME" --dbuser="$HLWP_WP_DB_USER" --dbpass="$HLWP_WP_DB_USER_PASSWORD" --dbhost="$HLWP_WP_DB_HOST" 2> error.log
+_HLWP_KILL_IF_ERROR "Could not create configuration for WordPress"
+
+# WP-CLI - Ensuring clean database
+_HLWP_LOG "Ensuring clean database ..."
+php wp-cli.phar db drop --yes 2> error.log
+_HLWP_KILL_IF_ERROR "Could not drop the current database"
+php wp-cli.phar db create 2> error.log
+_HLWP_KILL_IF_ERROR "Could not create a new database"
+
+# WP-CLI - Install WordPress
+_HLWP_LOG "Running WordPress installation process ..."
+php wp-cli.phar core install --url=localhost:8080 --title="$(_HLWP_WP_DEFAULT themename)" --admin_user="$HLWP_WP_USER" --admin_password="$HLWP_WP_PASSWORD" --admin_email="$HLWP_WP_EMAIL" --skip-email  2> error.log
+_HLWP_KILL_IF_ERROR "Could complete WordPress installation"
+
+# WP-CLI - Delete unnecessary files
+_HLWP_LOG "Deleting unnecessary files ..."
+php wp-cli.phar theme activate "$(_HLWP_WP_DEFAULT themedir)"
+php wp-cli.phar theme delete twentyfourteen
+php wp-cli.phar theme delete twentyfifteen
+php wp-cli.phar theme delete twentysixteen
+php wp-cli.phar theme delete twentyseventeen
+php wp-cli.phar plugin delete akismet
+php wp-cli.phar plugin delete hello
+
+# WP-CLI - Activating plugins
+_HLWP_LOG "Activating plugins ..."
+php wp-cli.phar plugin activate $(_HLWP_LIST_PLUGINS) 2> error.log
+_HLWP_KILL_IF_ERROR "Could not activate plugins"
+
+# WP-CLI - Sync ACF
+_HLWP_LOG "Sync ACF ..."
+php wp-cli.phar acf sync 2> error.log
+_HLWP_KILL_IF_ERROR "Could not sync ACF"
+
+# WP-CLI - Rewrite structure
+_HLWP_LOG "Rewrite structure ..."
+php wp-cli.phar rewrite structure "/%year%/%monthnum%/%day%/%postname%/" 2> error.log
+_HLWP_KILL_IF_ERROR "Could not edit rewrite structure"
+
+# WP-CLI - Adding some test data ...
+_HLWP_LOG "Adding some test data ..."
+php wp-cli.phar option update blogdescription "$(_HLWP_WP_DEFAULT wpdesc)"
+php wp-cli.phar post update 1 wordpress/wp-content/themes/postlight-headless-wp/post-content/sample-post.txt --post_title="Sample Post" --post_name=sample-post
+php wp-cli.phar post create wordpress/wp-content/themes/postlight-headless-wp/post-content/welcome.txt --post_type=page --post_status=publish --post_name=welcome --post_title="Congratulations!"
+php wp-cli.phar term update category 1 --name="Sample Category"
+php wp-cli.phar menu create "Header Menu"
+php wp-cli.phar menu item add-post header-menu 1
+php wp-cli.phar menu item add-post header-menu 2
+php wp-cli.phar menu item add-term header-menu category 1
+php wp-cli.phar menu item add-custom header-menu "Read about the Starter Kit on Medium" https://trackchanges.postlight.com/introducing-postlights-wordpress-react-starter-kit-a61e2633c48c
+php wp-cli.phar menu location assign header-menu header-menu
+
+# Installation complete
+_HLWP_LOG "Installation is now complete! You can now start your webserver and login." success
 
 # Clean up the variables
 _HLWP_UNSET_VARIABLES
--- library.sh
+++ library.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+# Clean up
+_HLWP_UNSET_VARIABLES()
+{
+    # Misc
+    unset HLWP_CONFIG_STATUS
+    unset HLWP_CONFIG_COMPLETE
+
+    # Input data
+    unset HLWP_WP_USER
+    unset HLWP_WP_PASSWORD
+    unset HLWP_WP_EMAIL
+    unset HLWP_WP_DB_HOST
+    unset HLWP_WP_DB_USER
+    unset HLWP_WP_DB_USER_PASSWORD
+    unset HLWP_WP_DB_NAME
+
+    # Functions
+    unset -f _HLWP_COLOR
+    unset -f _HLWP_WP_DEFAULT
+    unset -f _HLWP_LOG
+    unset -f _HLWP_KILL_IF_ERROR
+    unset -f _HLWP_CHECK_PREREQUISITES
+    unset -f _HLWP_READ_CONFIG
+    unset -f _HLWP_LIST_PLUGINS
+    unset -f _HLWP_UNSET_VARIABLES
+}
+
 # Colors
 _HLWP_COLOR()
 {
@@ -20,33 +47,57 @@ _HLWP_COLOR()
         lightpurple) printf "\033[1;35m" ;;
         lightcyan)   printf "\033[1;36m" ;;
         white)       printf "\033[1;37m" ;;
-        *)           printf "\033[0m";;
+        *)           printf "\033[0m" ;;
     esac
 }
 
-# Colors
+# Default config data
 _HLWP_WP_DEFAULT()
 {
     case $1 in
-        user)     printf "my_wp_user" ;;
-        password) printf "thismightbeagoodpassword" ;;
-        email)    printf "my_email@mywebsite.com" ;;
-        dbname)   printf "wp_headless" ;;
-        *)        printf "undefined" ;;
+        user)           printf "my_wp_user" ;;
+        password)       printf "thismightbeagoodpassword" ;;
+        email)          printf "my_email@mywebsite.com" ;;
+        dbhost)         printf "localhost" ;;
+        dbuser)         printf "hlwp-test" ;;
+        dbuserpassword) printf "qwerty" ;;
+        dbname)         printf "hlwp-test" ;;
+        themename)      printf "Postlight Headless WP Starter" ;;
+        themedir)       printf "postlight-headless-wp" ;;
+        wpdesc)         printf "Just another (headless) WordPress site" ;;
+        *)              printf "undefined" ;;
     esac
 }
 
 # Log function
 _HLWP_LOG()
 {
+    case $2 in
+        error)   local COLOR=$(_HLWP_COLOR lightred) ;;
+        warning) local COLOR=$(_HLWP_COLOR yellow) ;;
+        success) local COLOR=$(_HLWP_COLOR lightgreen) ;;
+        *)       local COLOR=$(_HLWP_COLOR lightcyan) ;;
+    esac
+
     local MESSAGE=$1
-    printf "$(_HLWP_COLOR lightcyan)[HLWP]$(_HLWP_COLOR) ${MESSAGE}\n"
+    
+    printf "${COLOR}[HLWP]$(_HLWP_COLOR) ${MESSAGE}\n"
+}
+
+# Error log function
+_HLWP_KILL_IF_ERROR()
+{
+    if [ -s ./error.log ]; then
+        _HLWP_LOG "$1 - check error.log for more information" error
+        kill -INT $$
+    fi
 }
 
 # Description
 _HLWP_CHECK_PREREQUISITES()
 {
-
+    rm error.log
+    echo "_HLWP_CHECK_PREREQUISITES goes here"
 }
 
 # Read config
@@ -56,67 +107,93 @@ _HLWP_READ_CONFIG()
 
     while [ $HLWP_CONFIG_STATUS == 0 ]
     do
-        _HLWP_LOG "Installation config:"
+        _HLWP_LOG "Installation config"
 
         echo
 
-        printf "   $(_HLWP_COLOR white)Username:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT user))$(_HLWP_COLOR) "
+        printf "   $(_HLWP_COLOR white)WordPress config$(_HLWP_COLOR)\n"
+        printf "    - $(_HLWP_COLOR white)Username:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT user))$(_HLWP_COLOR) "
         read HLWP_WP_USER
+        HLWP_WP_USER=${HLWP_WP_USER:-$(_HLWP_WP_DEFAULT user)}
 
-        printf "   $(_HLWP_COLOR white)Password:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT password))$(_HLWP_COLOR) "
+        printf "    - $(_HLWP_COLOR white)Password:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT password))$(_HLWP_COLOR) "
         read HLWP_WP_PASSWORD
+        HLWP_WP_PASSWORD=${HLWP_WP_PASSWORD:-$(_HLWP_WP_DEFAULT password)}
 
-        printf "   $(_HLWP_COLOR white)Email:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT email))$(_HLWP_COLOR) "
+        printf "    - $(_HLWP_COLOR white)Email:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT email))$(_HLWP_COLOR) "
         read HLWP_WP_EMAIL
-
-        printf "   $(_HLWP_COLOR white)Database name:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT dbname))$(_HLWP_COLOR) "
-        read HLWP_WP_DB_NAME
+        HLWP_WP_EMAIL=${HLWP_WP_EMAIL:-$(_HLWP_WP_DEFAULT email)}
 
         echo
 
-        printf "   Is the data above correct? ($(_HLWP_COLOR lightgreen)y$(_HLWP_COLOR) = Yes, continue / $(_HLWP_COLOR yellow)n$(_HLWP_COLOR) = No, edit config / $(_HLWP_COLOR lightred)c$(_HLWP_COLOR) = Cancel installation) "
-        read HLWP_CONFIG_COMPLETE
+        printf "   $(_HLWP_COLOR white)MySQL database config$(_HLWP_COLOR)\n"
+        printf "    - $(_HLWP_COLOR white)Database host:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT dbhost))$(_HLWP_COLOR) "
+        read HLWP_WP_DB_HOST
+        HLWP_WP_DB_HOST=${HLWP_WP_DB_HOST:-$(_HLWP_WP_DEFAULT dbhost)}
 
-        case $HLWP_CONFIG_COMPLETE in
-            "y")
-                HLWP_CONFIG_STATUS=1
-                echo
-                ;;
-            "n")
-                echo
-                ;;
-            *)
-                echo
-                _HLWP_LOG "Cancelling installation"
-                _HLWP_UNSET_VARIABLES
-                kill -INT $$
-                ;;
-        esac
+        printf "    - $(_HLWP_COLOR white)Username:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT dbuser))$(_HLWP_COLOR) "
+        read HLWP_WP_DB_USER
+        HLWP_WP_DB_USER=${HLWP_WP_DB_USER:-$(_HLWP_WP_DEFAULT dbuser)}
+
+        printf "    - $(_HLWP_COLOR white)Password:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT dbuserpassword))$(_HLWP_COLOR) "
+        read HLWP_WP_DB_USER_PASSWORD
+        HLWP_WP_DB_USER_PASSWORD=${HLWP_WP_DB_USER_PASSWORD:-$(_HLWP_WP_DEFAULT dbuserpassword)}
+
+        printf "    - $(_HLWP_COLOR white)Database name:$(_HLWP_COLOR) $(_HLWP_COLOR darkgray)($(_HLWP_WP_DEFAULT dbname))$(_HLWP_COLOR) "
+        read HLWP_WP_DB_NAME
+        HLWP_WP_DB_NAME=${HLWP_WP_DB_NAME:-$(_HLWP_WP_DEFAULT dbname)}
+
+        echo
+
+        HLWP_CONFIG_CONFIRM_STATUS=0;
+
+        while [ $HLWP_CONFIG_CONFIRM_STATUS == 0 ]
+        do
+
+            printf "   Is the data above correct? ($(_HLWP_COLOR lightgreen)y$(_HLWP_COLOR) = Yes, continue / $(_HLWP_COLOR yellow)n$(_HLWP_COLOR) = No, edit config / $(_HLWP_COLOR lightred)c$(_HLWP_COLOR) = Cancel installation) "
+            read HLWP_CONFIG_COMPLETE
+
+            case $HLWP_CONFIG_COMPLETE in
+                "y")
+                    HLWP_CONFIG_CONFIRM_STATUS=1
+                    HLWP_CONFIG_STATUS=1
+                    echo
+                    ;;
+                "n")
+                    HLWP_CONFIG_CONFIRM_STATUS=1
+                    echo
+                    ;;
+                "c")
+                    echo
+                    HLWP_CONFIG_CONFIRM_STATUS=1
+                    _HLWP_LOG "Cancelling installation"
+                    _HLWP_UNSET_VARIABLES
+                    kill -INT $$
+                    ;;
+                *)
+                    echo "   Invalid anwser"
+                    ;;
+                    
+            esac
+
+        done
         
     done
 }
 
-# Clean up
-_HLWP_UNSET_VARIABLES()
+_HLWP_LIST_PLUGINS()
 {
-    # Misc
-    unset HLWP_CONFIG_STATUS
-    unset HLWP_CONFIG_COMPLETE
+    local PLUGINS=""
 
-    # Input data
-    unset HLWP_WP_USER
-    unset HLWP_WP_PASSWORD
-    unset HLWP_WP_EMAIL
-    unset HLWP_WP_THEME_DIR
-    unset HLWP_WP_THEME_NAME
-    unset HLWP_WP_DESC
-    unset HLWP_WP_DB_NAME
+    for plugin in "./wordpress/wp-content/plugins"/*
+    do
+        plugin=${plugin%*/}
+        if [ ${plugin##*/} != "index.php" ]; then
+            PLUGINS="$PLUGINS ${plugin##*/}"
+        fi
 
-    # Functions
-    unset -f _HLWP_COLOR
-    unset -f _HLWP_WP_DEFAULT
-    unset -f _HLWP_LOG
-    unset -f _HLWP_CHECK_PREREQUISITES
-    unset -f _HLWP_READ_CONFIG
-    unset -f _HLWP_UNSET_VARIABLES
+        unset plugin
+    done
+
+    echo $PLUGINS
 }
