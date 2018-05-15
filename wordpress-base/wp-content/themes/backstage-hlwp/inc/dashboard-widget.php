588696f26fcde7b4b51b8434a69cfd4ef8332486
pthorsson <p.thorsson@gmail.com>
2018-05-15 22:49:16 +0200

    Fixed front end preview function

--- wordpress-base/wp-content/themes/backstage-hlwp/inc/dashboard-widget.php
+++ wordpress-base/wp-content/themes/backstage-hlwp/inc/dashboard-widget.php
@@ -19,14 +19,14 @@ add_action( 'wp_dashboard_setup', 'hlwp_preview_link_widget' );
  * Create the function to output the contents of our Dashboard Widget.
  */
 function hlwp_preview_link_function() {
-    $restNonce = wp_create_nonce( 'hlwp_content_preview' );
+    $restNonce = wp_create_nonce( 'wp_rest' );
 
     ?>
         <p>
             You can open your front end application in preview mode allowing it to fetch unpublished content from Backstage.
         </p>
         <div style="padding-top: 10px; float: right; height: 30px;">
-            <a href="http://localhost:9001?nonce-token=<?php echo $restNonce ?>" target="_blank" class="button button-primary">Open preview mode</a>
+            <a href="http://localhost:9001?_wpnonce=<?php echo $restNonce ?>" target="_blank" class="button button-primary">Open preview mode</a>
         </div>
         <div style="padding-top: 15px; height: 25px; overflow: hidden;">
             Token: <code><?php echo $restNonce ?></code>
